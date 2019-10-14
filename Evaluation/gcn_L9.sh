fde2a057ac3d4d695436478d942d867376133048
johncava <jcava@asu.edu>
2019-10-14 04:16:30 -0700

    scripts for 9 layers

---
+++ Evaluation/gcn_L9.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -p gpu                      # Use gpu partition
+#SBATCH -q wildfire                 # Run job under wildfire QOS queue
+
+#SBATCH --gres=gpu:1                # Request two GPUs
+
+#SBATCH -t 0-24:00                  # wall time (D-HH:MM)
+#SBATCH -o gcn_L9.out             # STDOUT (%j = JobId)
+#SBATCH -e gcn_L9.err             # STDERR (%j = JobId)
+#SBATCH --mail-type=ALL             # Send a notification when a job starts, stops, or fails
+#SBATCH --mail-user=jcava@asu.edu # send-to address
+
+source activate /home/jcava/.conda/envs/pytorch-1.20-gpu/
+python gcn_L9.py
+conda deactivate
